cd22d0c8bfeebac23a579bf9285c7234f0a2ae1b
Jatin Katyal <jatin.katyal13@gmail.com>
2018-07-02 22:26:04 +0530

    Initiated

---
+++ detect.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+	print("Error opening")
+
+while cap.isOpened():
+	ret, frame = cap.read()
+
+	gray = cv2.cvtColor(frame, cv2.COLOR_RGB2)
+
+	if ret:
+		cv2.imshow('Frame', gray)
+		
+		if cv2.waitKey(25) & 0xff == ord('q'): break
+
+
+cap.release()
+
+cv2.destroyAllWindows()
